8da5a75e6cb1463a038c89f7fc9bea5b5a19288f
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-08 22:48:18 +0330

    GS5: Switch to Wifi service AIDL
    Change-Id: I5a2864b4159b7815eff4dd2f0de02a3dabbacfa5

--- device.mk
+++ device.mk
@@ -460,7 +460,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy.GS5
+    android.hardware.wifi-service-lazy.GS5
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
